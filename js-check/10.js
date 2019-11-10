ff5b5016bbe42d87211ea80174956f84e7af768b
Yulia Kotenko <julkotenko@gmail.com>
2019-11-10 18:03:49 +0200

    Fixed task 10

--- js-check/10.js
+++ js-check/10.js
@@ -5,54 +5,94 @@ class Storage {
 
   list() {
     return new Promise((resolve, reject) => {
-      Object.keys(this.storage) ?  resolve(console.log(Object.keys(this.storage))) : reject(console.log(new Error("Storage is empty")));
+      const listKeys = Object.keys(this.storage);
+      if (!listKeys) {
+        reject(new Error("Storage is empty"));
+        return;
+      }
+      resolve(listKeys);
     });
   }
 
   fetch(key) {
     return new Promise((resolve, reject) => {
-      this.storage[key] ? resolve(console.log(this.storage[key])) : reject(console.log(new Error(`Data of ${key} is empty`)));
+      if (!key) {
+        reject(new Error(`${key} is empty`));
+        return;
+      }
+      const dataValue = this.storage[key];
+      resolve(dataValue);
     });
   }
 
   store(key, data) {
     return new Promise((resolve, reject) => {
+      if (!key || !data) {
+        reject(new Error(`Data of ${key} is empty or ${key} is not exist`));
+        return;
+      }
       this.storage[key] = data;
-      resolve(console.log(this.storage));
+      resolve(this.storage);
     });
   }
 
   destroy(key) {
     return new Promise((resolve, reject) => {
-      if(this.storage[key]) {
-        delete this.storage[key];
-        resolve(console.log(this.storage));
-      } else reject(new Error("This key isn't exist"));
+      if(!key) {
+        reject(new Error("This key isn't exist"));
+        return;
+      }
+      delete this.storage[key];
+      resolve(this.list());
     });
   }
 
-  storeList([{key, data}]) {
+  storeList(params) {
     return new Promise((resolve, reject) => {
-      [{key, data}] ? resolve(console.log([{key, data}].forEach(({key, data}) => this.store(key, data)))) : reject(new Error("Storage is empty"));
+      if (Array.isArray(params) === false) {
+        reject(new Error("It isn't array"));
+        return;
+      }
+      params.forEach((item) => this.store(item.key,item.data).then((res) => resolve(res)));
+    });
+  }
+
+  destroyStartedWith(beginningOfKey) {
+    return new Promise((resolve, reject) => {
+      if (!beginningOfKey) {
+        reject(new Error("Parameter is empty"));
+        return;
+      }
+      const reg = new RegExp(`^${beginningOfKey}`, 'gi');
+      this.list().then((res) => {
+        res.forEach((item) => {
+          if (reg.test(item) === true) {
+            this.destroy(item).then();
+            resolve('Done');
+          }
+        });
+      });
     });
   }
 
   fetchInTimeOrFail(key, timeout) {
-    return new Promise(function(resolve, reject) {
-      resolve(console.log(this.storage[key]));
-      setTimeout(() => {
-        reject(new Error("Search time out"));
-      }, timeout);
+    return new Promise((resolve, reject) => {
+      if (key && timeout) {
+        this.fetch(key).then((res) => resolve(res));
+        setTimeout(() => {
+          reject(new Error("Search time out"));
+        }, timeout);
+      }
     });
   }
 }
 
-
-const newStorage = new Storage();
-newStorage.store("key1", "data1").then();
-newStorage.store("key2", "data2").then();
-newStorage.list().then();
-newStorage.fetch("key2").then();
-newStorage.fetchInTimeOrFail("key2", 100).then();
-newStorage.storeList([{key: "key1", data: "data1"}, {key: "key2", data: "data2"}]).then();
-newStorage.destroy("key1").then();
+let newStorage = new Storage();
+newStorage.store("key1", "data1").then((res) => console.log(res)).catch((error) => console.log(error));
+newStorage.store("key2", "data2").then((res) => console.log(res)).catch((error) => console.log(error));
+newStorage.list().then((res) => console.log(res)).catch((error) => console.log(error));
+newStorage.fetch("key2").then((res) => console.log(res)).catch((error) => console.log(error));
+newStorage.fetchInTimeOrFail("key1", 100).then((res) => console.log(res)).catch((error) => console.log(error));
+newStorage.storeList([{key: "key11", data: "data11"}, {key: "key22", data: "data22"}]).then((res) => console.log(res)).catch((error) => console.log(error));
+newStorage.destroy("key2").then((res) => console.log(res)).catch((error) => console.log(error));
+newStorage.destroyStartedWith('key').then((res) => console.log(res)).catch((error) => console.log(error));
