35b1c9c9a64594706b46d033774b99ebb4829795
Yulia Kotenko <julkotenko@gmail.com>
2019-11-18 23:09:46 +0200

    Replaced .then/.catch with try/catch

--- homework-02/src/app.js
+++ homework-02/src/app.js
@@ -2,7 +2,12 @@ const { task1: amount, task2: info, task3: result } = require('./task');
 
 const boot = async () => {
   console.log(amount.sum);
-  await result.then((res) => console.log(res)).catch((error) => console.log(error));
+  try {
+    const res = await result;
+    console.log(res);
+  } catch (error) {
+    console.log(error);
+  }
   info.planetInfo();
 };
 
