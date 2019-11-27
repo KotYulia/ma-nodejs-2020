8e976eda701cd787b9ca30a1c061fc2db05df8e4
Yulia Kotenko <julkotenko@gmail.com>
2019-11-27 23:28:28 +0200

    Removed cycle for

--- homework-03/src/index.js
+++ homework-03/src/index.js
@@ -1,21 +1,24 @@
 let count = 2;
-let primeNumber = 2;
-
-const checkPrimeNumber = (number) => {
-  for (let i = 2; i <= Math.sqrt(number); i++) {
-    if (number % i === 0) return false;
-  }
-  return true;
-};
+let numberToCheck = 4;
+let maxPrimeNumber = 3;
 
 setInterval(() => {
-  if (checkPrimeNumber(count)) {
-    primeNumber = count;
+  if (numberToCheck % count === 0) {
+    numberToCheck++;
+    count = 2;
+    return;
   }
+
+  if (count > Math.sqrt(numberToCheck)) {
+    maxPrimeNumber = numberToCheck++;
+    count = 2;
+    return;
+  }
+
   count++;
 }, 0);
 
 setInterval(() => {
   const timestamp = Math.floor(Date.now() / 1000);
-  console.log(`${timestamp}-- IN PROCESS -- Biggest prime number found: ${primeNumber}`);
+  console.log(`${timestamp}-- IN PROCESS -- Biggest prime number found: ${maxPrimeNumber}`);
 }, 1000);
