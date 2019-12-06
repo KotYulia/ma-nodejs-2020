da2465e494ada645eb068d6cc27a403a70ac8485
KotJulia <julkotenko@gmail.com>
2019-12-06 11:09:19 +0200

    Changes according to the DRY rule

--- homework-04/src/callback-func.js
+++ homework-04/src/callback-func.js
@@ -1,25 +1,25 @@
-const throwDice = (throwDiceAction) => {
-  const randomNum = Math.floor(Math.random() * 7);
-  const errorMsg = 'Lost dice!';
-  if (randomNum === 0) {
-    return throwDiceAction(new Error(errorMsg));
-  }
-  return throwDiceAction(null, randomNum);
-};
-
 const throwDiceResult = () => {
   return Math.floor(Math.random() * 7);
 };
 
+const throwDice = (throwDiceAction) => {
+  const throwResult = throwDiceResult();
+  const errorMsg = 'Lost dice!';
+  if (throwResult === 0) {
+    return throwDiceAction(new Error(errorMsg));
+  }
+  return throwDiceAction(null, throwResult);
+};
+
 const start = (timeStep) => {
   return new Promise((resolve, reject) => {
-    const randomNum = throwDiceResult();
-    if (randomNum === 0) {
+    const throwResult = throwDiceResult();
+    if (throwResult === 0) {
       reject(new Error('Lost dice!'));
       return;
     }
     setTimeout(() => {
-      resolve(randomNum);
+      resolve(throwResult);
     }, timeStep);
   });
 };
